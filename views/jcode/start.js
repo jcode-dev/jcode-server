0cdec07e07dd3cc49a3c6d9aaa828b861e768ef4
Koichi Ito <koichii@live.jp>
2017-09-23 22:16:52 +0900

    update

--- views/jcode/start.js
+++ views/jcode/start.js
@@ -1,6 +1,6 @@
 inst = new JCODE.instruction();
 inst.html('こんにちは、プログラミング<ruby>教育研究所<rp>(</rp><rt>きょういくけんきゅうじょ</rt><rp>)</rp></ruby>にようこそ。<br />');
-inst = new JCODE.instruction('この<button>ボタン</button>を押してみて');
+inst = new JCODE.instruction('こんなふうに、プログラムで、たまを動かしてみよう');
 
 $.ajax({
     url: "/home/toolbox.xml",
@@ -13,7 +13,8 @@ $.ajax({
   alert("読み込みに失敗しました。");
 });
 
-var obj = new JCODE.obj("pin");
-for (var count = 0; count < 3; count++) {
+var obj = new JCODE.obj("sphere");
+for (var count = 0; count < 4; count++) {
+  obj.moveForward(10);
   obj.turnRight(90);
 }

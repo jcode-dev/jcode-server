79a889ef9fe5b2c36469f98d55d3c7eaf6bc8e57
Koichi Ito <koichii@live.jp>
2017-10-08 09:32:44 +0900

    update

--- app.js
+++ app.js
@@ -41,7 +41,7 @@ app.use('/physics', express.static(path.join(__dirname, '../physics')));
 app.use('/ace', express.static(path.join(__dirname, '../ace-builds')));
 app.use('/bootstrap', express.static(path.join(__dirname, '../bootstrap-3.3.7-dist')));
 //app.use('/lessons', express.static(path.join(__dirname, '../lessons')));
-app.use('/', express.static(path.join(__dirname, '../www')));
+app.use('/', express.static(path.join(__dirname, '../www'), {extensions: ['htm', 'html']}));
 
 // Configuring Passport
 var passport = require('passport');

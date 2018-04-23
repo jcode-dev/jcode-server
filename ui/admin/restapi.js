35d9300a089d3d9f7f8d0c32b9c0e428b2854124
Koichi Ito <kito@j-code.org>
2018-04-23 08:26:19 +0000

    update

---
+++ ui/admin/restapi.js
@@ -0,0 +1,27 @@
+"use strict";
+
+var restapi = {};
+
+restapi.base_url = location.protocol+"//"+location.host+"/api/";
+// axios with token
+restapi.axios = axios.create({ baseURL: restapi.base_url });
+restapi.axios.interceptors.request.use((config) => {
+	var token = localStorage.getItem('token');
+  if (token) {  
+    config.headers.Authorization = `Bearer ${token}`
+    return config
+  }
+  return config
+}, function (error) {
+  return Promise.reject(error)
+});
+
+restapi.get = function(api) {
+	return restapi.axios.get(restapi.base_url+api);
+}
+restapi.post = function(api, data) {
+	return restapi.axios.post(restapi.base_url+api, data);
+}
+restapi.delete = function(api) {
+	return restapi.axios.delete(restapi.base_url+api);
+}
